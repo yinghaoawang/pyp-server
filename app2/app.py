a4bf51229aaae2da93df8c4e49d46e734a289e49
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-05-28 16:45:16 -0700

    add request on app2 to connect to app1 (not working)

--- app2/app.py
+++ app2/app.py
@@ -1,7 +1,9 @@
+import requests
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Goodbye, World!'
+    response = requests.get('http://0.0.0.0:5002/')
+    return "Gottem"
