cada4e643111e4867c5c3ca26bc41f1ce4d42a58
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-06-04 17:54:34 -0700

    add auth skeleton w/ jwt

--- auth-service/index.js
+++ auth-service/index.js
@@ -1,15 +1,30 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 
-// Create an Express application
+const config = require('./config');
+
 const app = express();
+const { secretKey } = config;
 
-// Define a route
-app.get('/', (req, res) => {
-  res.json({ res: 'Hello, Node World!' });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  if (validateCredentials(username, password)) {
+    const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+
+    res.status(200).json({ token });
+  } else {
+    res.status(401).json({ message: 'Invalid login credentials' });
+  }
 });
 
-// Start the server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+function validateCredentials(username, password) {
+  return username === 'admin' && password === 'password';
+}
+
+app.listen(3000, () => {
+  console.info('Server started on port 3000');
 });
