966f38715748f287d37c04f791ead2ab9e38ce21
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-06-07 12:28:31 -0700

    add check for user in lobby

--- game-server/lobby/lobbySocketHandler.js
+++ game-server/lobby/lobbySocketHandler.js
@@ -1,4 +1,4 @@
-const { getAvailableLobbies, createLobby, joinLobby } = require('./LobbyManager');
+const lobbyManager = require('./LobbyManager');
 
 module.exports = {
   addListeners: (io, socket) => {
@@ -7,20 +7,32 @@ module.exports = {
     }
 
     socket.emit('getLobbies', {
-      lobbies: getAvailableLobbies()
+      lobbies: lobbyManager.getAvailableLobbies()
     });
 
     socket.on('createLobby', () => {
-      const lobby = createLobby([getUser()]);
-      socket.emit('getLobbies', {
-        lobbies: getAvailableLobbies()
-      });
+      if (lobbyManager.isUserInAnyLobby(getUser().id)) {
+        const errorMessage = 'Cannot create lobby, user is already in one';
+        socket.emit('error', { message: errorMessage });
+        console.error(errorMessage);
+        return;
+      }
 
-      joinLobby(lobby.id , getUser());
+      const lobby = lobbyManager.createLobby(getUser());
+      socket.emit('getLobbies', {
+        lobbies: lobbyManager.getAvailableLobbies()
+      });
       socket.emit('joinLobby', { lobbyId: lobby.id });
     });
 
     socket.on('joinLobby', (payload) => {
+      if (lobbyManager.isUserInAnyLobby(getUser().id)) {
+        const errorMessage = 'Cannot join lobby, user is already in one';
+        socket.emit('error', { message: errorMessage });
+        console.error(errorMessage);
+        return;
+      }
+
       const { lobbyId } = payload;
 
       joinLobby(lobbyId, getUser());
