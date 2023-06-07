6c3209737d7638848d4b2d5b2aeb9b424cc5567b
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-06-07 12:28:16 -0700

    refactor lobby manager to a singleton class

--- game-server/lobby/LobbyManager.js
+++ game-server/lobby/LobbyManager.js
@@ -1,50 +1,70 @@
-const { 
-  v4: uuidv4,
-} = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const Lobby = require('./Lobby');
 
 const lobbies = new Map();
 
-module.exports = {
-  createLobby: (users = []) => {
+class LobbyManager {
+  createLobby(user) {
     const lobbyId = uuidv4();
     const lobby = new Lobby(lobbyId);
     if (lobbies.get(lobbyId) != null) {
       throw new Error('Lobby ID is not unique');
-    } 
+    }
     lobbies.set(lobbyId, lobby);
-    lobby.addUsers(users);
+    lobby.addUser(user);
 
     return lobby;
-  },
+  }
 
-  getLobby: (lobbyId) => {
+  isUserInLobby(lobbyId, userId) {
+    const lobby = lobbies.get(lobbyId);
+    if (lobby == null) {
+      throw new Error('Lobby does not exist');
+    }
+    return lobby.getUser(userId) != null;
+  }
+
+  isUserInAnyLobby(userId) {
+    for (const [key, value] of lobbies.entries()) {
+      const lobbyId = key;
+      console.log(`checking if ${userId} is in ${lobbyId}`);
+      if (this.isUserInLobby(lobbyId, userId)) return true;
+    }
+    return false;
+  }
+
+  getLobby(lobbyId) {
     return lobbies.get(lobbyId);
-  },
+  }
 
-  getLobbies: () => {
+  getLobbies() {
     return lobbies;
-  },
+  }
 
-  joinLobby: (lobbyId, user) => {
+  joinLobby(lobbyId, user) {
     const lobby = lobbies.get(lobbyId);
     if (lobby == null) {
       throw new Error('Lobby does not exist');
     }
 
     lobby.addUser(user);
-  },
+  }
 
-  leaveLobby: (lobbyId, user) => {
+  leaveLobby(lobbyId, user) {
     const lobby = lobbies.get(lobbyId);
     if (lobby == null) {
       throw new Error('Lobby does not exist');
     }
     lobby.removeUser(user.id);
-  },
+  }
 
-  getAvailableLobbies: () => {
+  getAvailableLobbies() {
     return Array.from(lobbies.keys());
   }
-};
+}
+
+/** Singleton instance the lobby manager class */
+const instance = new LobbyManager();
+
+module.exports = instance;
